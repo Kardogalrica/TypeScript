a4c29c8885fbef165996c3a08a0124ecc8339b1c
Galriça <rgalrica@kpmg.com>
2026-01-03 12:00:41 +0000

    enviado Tipo dados basico

--- app.ts
+++ app.ts
@@ -1,7 +1,21 @@
+/* 
+Aula 1 - Executando ts
 function helloWorld(nome:string) {
     return "Hello World " + nome;
 }
 
 let nome = "Ricardo"
 
-console.log(helloWorld(nome))
+console.log(helloWorld(nome)) */
+
+
+/* Aula 2 -  Tipo de Dados Basicos
+
+let carro:string = "Ferrari"
+let rodas:number = 4
+let temTetoSolar:boolean = true
+
+console.log(carro)
+console.log(rodas)
+console.log(temTetoSolar) */
+
