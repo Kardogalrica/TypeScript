b869faf2ee32435635dc72459957af349286d00f
Galriça <rgalrica@kpmg.com>
2026-01-03 12:43:32 +0000

    tipificação de arrys e objetos

--- app.ts
+++ app.ts
@@ -19,3 +19,17 @@ console.log(carro)
 console.log(rodas)
 console.log(temTetoSolar) */
 
+/* Aula 3 - Objectos e Arrys
+
+let frutas: string[] = ['Banana', 'Maçã','Laranja']
+let numeros: Array<number> = [1,2,3,4]
+
+let obj = {
+    name:"Ricardo",
+    age:32
+}
+
+frutas.push(1)
+numeros.push('ola')
+
+obj.name = 2 */
